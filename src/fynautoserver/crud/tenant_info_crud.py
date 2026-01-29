014e079ad158142777deea6866d6324847943b22
YuvrajFyn <yuvrajrathore1203@gmail.com>
2026-01-29 11:14:15 +0530

    Update tenant_info_crud.py

--- src/fynautoserver/crud/tenant_info_crud.py
+++ src/fynautoserver/crud/tenant_info_crud.py
@@ -35,9 +35,9 @@ async def add_tenant(payload:AddTenantModel):
             destination_folder_Assets = f"./src/tenant/tenants/{payload.tenancyName}"
             shutil.copytree(source_folder_Assets, destination_folder_Assets,dirs_exist_ok=True)
             
-            font_light_folder = f"tenant/tenants/{payload.tenancyName}/assets/fonts/AppFont/Quicksand Light.ttf"
-            font_regular_folder = f"tenant/tenants/{payload.tenancyName}/assets/fonts/AppFont/Quicksand SemiBold.ttf"
-            font_bold_folder = f"tenant/tenants/{payload.tenancyName}/assets/fonts/AppFont/Quicksand Bold.ttf"
+            font_light_folder = f"tenant/tenants/{payload.tenancyName}/assets/fonts/AppFont/Quicksand-Light.ttf"
+            font_regular_folder = f"tenant/tenants/{payload.tenancyName}/assets/fonts/AppFont/Quicksand-SemiBold.ttf"
+            font_bold_folder = f"tenant/tenants/{payload.tenancyName}/assets/fonts/AppFont/Quicksand-Bold.ttf"
             #create
             fonts=Fonts(
                 tenantId=payload.tenantId,
